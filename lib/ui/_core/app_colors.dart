585600d638106236be2784e75fa293f3f148b460
thomassantospallarini <thomassantospallarini@gmail.com>
2025-04-14 20:51:08 +0000

    setting colors and themes

--- lib/ui/_core/app_colors.dart
+++ lib/ui/_core/app_colors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 abstract class AppColors {
   static const Color backgroundColor = Color(0xFF202123);
+  static const Color mainColor = Color(0xFFffa559);
 }
