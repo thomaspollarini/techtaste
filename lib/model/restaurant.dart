3d5c79df9e5598b4e5bc2b2726555f2c75946412
thomassantospallarini <thomassantospallarini@gmail.com>
2025-04-16 12:54:36 +0000

    upt restaurant to get dishes

--- lib/model/restaurant.dart
+++ lib/model/restaurant.dart
@@ -1,3 +1,5 @@
+import 'package:myapp/model/dish.dart';
+
 class Restaurant {
   String id;
   String imagePath;
@@ -6,6 +8,7 @@ class Restaurant {
   double stars;
   int distance;
   List<String> categories;
+  List<Dish> dishes;
 
   Restaurant({
     required this.id,
@@ -15,6 +18,7 @@ class Restaurant {
     required this.stars,
     required this.distance,
     required this.categories,
+    required this.dishes,
   });
 
   Map<String, dynamic> toMap() {
@@ -26,6 +30,7 @@ class Restaurant {
       'stars': stars,
       'distance': distance,
       'categories': categories,
+      'dishes': dishes.map((dish) => dish.toMap()).toList(),
     };
   }
 
@@ -38,11 +43,12 @@ class Restaurant {
       stars: map['stars'],
       distance: map['distance'],
       categories: List<String>.from(map['categories']),
+      dishes: List<Dish>.from(map['dishes'].map((dish) => Dish.fromMap(dish))),
     );
   }
 
   @override
   String toString() {
-    return 'Restaurant{id: $id, imagePath: $imagePath, name: $name, description: $description, stars: $stars, distance: $distance, categories: $categories}';
+    return 'Restaurant{id: $id, imagePath: $imagePath, name: $name, description: $description, stars: $stars, distance: $distance, categories: $categories, dishes: $dishes}';
   }
 }
