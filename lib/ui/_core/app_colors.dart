091240244042b70cf8cb1f21521ea48650aa4c0e
thomassantospallarini <thomassantospallarini@gmail.com>
2025-04-16 14:12:35 +0000

    new text colors added

--- lib/ui/_core/app_colors.dart
+++ lib/ui/_core/app_colors.dart
@@ -4,4 +4,6 @@ abstract class AppColors {
   static const Color backgroundColor = Color(0xFF202123);
   static const Color mainColor = Color(0xFFffa559);
   static const Color lightBackgroundColor = Color(0xFF343541);
+  static const Color highlightText = Color(0xFFffe6c7);
+  static const Color textCards = Color(0xFFdbd7df);
 }
